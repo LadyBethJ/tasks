1a58ee61372bf5c38d4eef1554348d13d11661a0
LadyBethJ <maribel.juradobaena@gmail.com>
2021-07-31 15:42:55 +0200

    First commit. Adding project

---
+++ app/src/main/java/com/mjb/tasks/FullscreenActivity.kt
@@ -0,0 +1,57 @@
+package com.mjb.tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.os.postDelayed
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class FullscreenActivity : AppCompatActivity() {
+    private lateinit var fullscreenContent: TextView
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_fullscreen)
+
+        supportActionBar?.hide()
+
+        fullscreenContent = findViewById(R.id.title)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            this.finish()
+        }, AUTO_HIDE_DELAY_MILLIS.toLong())
+    }
+
+    companion object {
+        /**
+         * Whether or not the system UI should be auto-hidden after
+         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+         */
+        private const val AUTO_HIDE = true
+
+        /**
+         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+         * user interaction before hiding the system UI.
+         */
+        private const val AUTO_HIDE_DELAY_MILLIS = 3000
+
+        /**
+         * Some older devices needs a small delay between UI widget updates
+         * and a change of the status and navigation bar.
+         */
+        private const val UI_ANIMATION_DELAY = 300
+    }
+}
